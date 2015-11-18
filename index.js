707c716b5643417a87dedf6166a6be002a7b530b
Przemysław Płyś <przemyslaw.plys@gmail.com>
2015-11-18 17:14:49 +0100

    Added mongoDB and mongooose module.

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 console.log('IoTHuB REST server.');
 
 var express = require('express');
+
+var mongoose = require('mongoose');
+
+
+
+
 var app = express();
 
 var config = {
-  port: 9000
+  port: 9000,
+    database:{
+        host: 'localhost',
+        port:27017,
+        name: 'IoTHuB',
+        /**
+         * @TODO Add user and password
+         */
+    }
 };
+mongoose.connect('mongodb://'+config.database.host+':'+ config.database.port + '/' +config.database.name);
+
 
 app.get('/', function (req, res) {
     res.send('IoTHuB REST server.');
