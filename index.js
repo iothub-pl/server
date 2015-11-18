325f2004fc6f4b0a165821de2bf7986af2240075
Przemysław Płyś <przemyslaw.plys@gmail.com>
2015-11-18 15:50:22 +0100

    Added express module and sample server.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 console.log('IoTHuB REST server.');
+
+var express = require('express');
+var app = express();
+
+var config = {
+  host: 9000
+};
+app.get('/', function (req, res) {
+    res.send('IoTHuB REST server.');
+});
+
+
+app.listen(config.host);
