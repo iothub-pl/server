21876e981c17070489b747d15b7faeb4f6792bdf
Przemysław Płyś <przemyslaw.plys@gmail.com>
2015-11-28 15:14:29 +0100

    Removed from gulpfile apidoc task

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,7 +2,6 @@
 var gulp = require('gulp');
 var nodemon = require('gulp-nodemon');
 var mocha = require('gulp-mocha');
-var     apidoc = require('gulp-apidoc');
 
 /**
  *
@@ -33,14 +32,4 @@ gulp.task('serve', function () {
 
 gulp.task('test', function () {
     gulp.src('./mvc/**/*test.js').pipe(mocha());
-});
-
-
-gulp.task('apidoc', function(done){
-    apidoc({
-        src: './mvc/**/controller.js',
-        dest: './apidoc/',
-        debug: true,
-
-    },done);
 });
