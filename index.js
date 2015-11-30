8304dea957acb740de9219b1784dde700d13ac53
Przemysław Płyś <przemyslaw.plys@gmail.com>
2015-12-01 00:38:32 +0100

    Added bodyParser module for POST data parsing

--- index.js
+++ index.js
@@ -3,12 +3,21 @@
 var app = require('express')(),
     mongoose = require('mongoose'),
     config = require('./config/config'),
-    log = require('./log');
+    log = require('./log'),
+    bodyParser = require('body-parser');
+
 
 log.log('IoTHuB REST server.');
 
 mongoose.connect('mongodb://' + config.database.host + ':' + config.database.port + '/' + config.database.name);
 
+
+
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
 
 require('./config/routes')(app);
 
@@ -16,6 +25,7 @@ var db = mongoose.connection;
 db.on('error', () => {
     console.error.bind(console, 'connection error:');
 });
+
 
 db.once('open', () => {
     log.log('Connection to database established.');
