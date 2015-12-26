98f0682915b25155105dac8870730ac84459bf3b
Przemysław Płyś <przemyslaw.plys@gmail.com>
2015-12-26 15:04:21 +0100

    Added documenttion

--- config/routes.js
+++ config/routes.js
@@ -1,16 +1,14 @@
 'use strict';
-var log = require('./../log');
-
+var express = require('express');
 module.exports = function (app) {
-    app.all('/*', function (req, res, next) {
-        // log.log('Url: ' + req.originalUrl);
-            next();
-    });
-    app.get('/', function (req, res) {
-        res.send('IoTHuB REST server - unknown request.');
-    });
+    app.use('/',  express.static('./apidoc'));
 
     app.use('/things', require('./../mvc/thing/routes'));
     app.use('/accounts', require('./../mvc/account/routes'));
     app.use('/authentication', require('./../mvc/authentication/routes'));
+    //app.all('/*', function (req, res, next) {
+    //    next();
+    //});
+
+
 };
