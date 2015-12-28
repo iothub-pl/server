fd47ba889dcf3f2547fce78538cb81de14d40093
Przemysław Płyś <przemyslaw.plys@gmail.com>
2015-12-28 01:25:47 +0100

    Cleaned main routes

--- config/routes.js
+++ config/routes.js
@@ -1,14 +1,14 @@
 'use strict';
 var express = require('express');
 module.exports = function (app) {
-    app.use('/',  express.static('./apidoc'));
-
+    /**
+     * Docuemtation
+     */
+    app.use('/', express.static('./apidoc'));
+    /**
+     * Application
+     */
     app.use('/things', require('./../mvc/thing/routes'));
     app.use('/accounts', require('./../mvc/account/routes'));
     app.use('/authentication', require('./../mvc/authentication/routes'));
-    //app.all('/*', function (req, res, next) {
-    //    next();
-    //});
-
-
 };
