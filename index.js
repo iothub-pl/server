93e3702bc0634cc7753ab56bd789c1997a33c890
Przemysław Płyś <przemyslaw.plys@gmail.com>
2016-01-03 02:44:48 +0100

    Added additional modules

--- index.js
+++ index.js
@@ -4,19 +4,45 @@ var app = require('express')(),
     mongoose = require('mongoose'),
     config = require('./config/config'),
     bodyParser = require('body-parser'),
-    passport = require('passport');
+    passport = require('passport'),
+    helmet = require('helmet'),
+    responseTime = require('response-time'),
+    compression = require('compression');
+
 
 if (config.ENVIROMENT === 'developement') {
+    /**
+     * https://github.com/expressjs/morgan
+     */
     var morgan = require('morgan');
     app.use(morgan('combined'));
 }
+/**
+ * https://github.com/expressjs/cors
+ */
 app.use(require('cors')());
-
-mongoose.connect('mongodb://' + config.DATABASE.HOST + ':' + config.DATABASE.PORT + '/' + config.DATABASE.DB);
+/**
+ * https://github.com/helmetjs/helmet
+ */
+app.use(helmet());
+/**
+ * https://github.com/expressjs/response-time
+ */
+app.use(responseTime());
+/**
+ * https://github.com/expressjs/body-parser
+ */
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 app.use(bodyParser.json());
+/**
+ * https://github.com/expressjs/compression
+ */
+app.use(compression());
+
+mongoose.connect('mongodb://' + config.DATABASE.HOST + ':' + config.DATABASE.PORT + '/' + config.DATABASE.DB);
+
 require('./mvc/authentication/passport').setup(app);
 require('./config/routes')(app);
 
