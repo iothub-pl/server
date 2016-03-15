068c00104baf00612ed6fbe9be78e0e0972792b7
Przemysław Płyś <przemyslaw.plys@gmail.com>
2016-03-16 00:37:25 +0100

    Fixed error - not calling next() inn middleware

--- middlewares/pagination.js
+++ middlewares/pagination.js
@@ -3,4 +3,5 @@ var config = require('./../configs/app');
 module.exports = (req, res, next)=> {
     req.query.skip = Math.abs(parseInt(req.query.skip) || config.REQ.QUERY.DEFAULT.SKIP);
     req.query.limit = Math.abs(parseInt(req.query.limit) || config.REQ.QUERY.DEFAULT.LIMIT);
+    next();
 };
