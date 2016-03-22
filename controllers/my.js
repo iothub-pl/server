b39bf78b190d24ca4332c6fbc93010ee18a05970
Przemysław Płyś <przemyslaw.plys@gmail.com>
2016-03-22 01:05:47 +0100

    Refactored apidoc documentation for my endpoints

--- controllers/my.js
+++ controllers/my.js
@@ -5,17 +5,20 @@ var Account = require('./../models/account'),
     winston = require('winston');
 
 /**
- * @api {get} /my/account Returns authenticated account
- * @apiDescription Returns authenticated account
+ * @api {get}   /my/account Returns authenticated account
+ * @apiDescription  Returns authenticated account
  * @apiName GetAccount
- * @apiGroup My
+ * @apiGroup    My
  *
- * @apiPermission user
- * @apiHeader {String} Authorization bearer User unique access-key.
+ * @apiPermission   user
+ * @apiUse  AuthenticationToken
  *
- * @apiSuccess (200) {String} _id   Id of the User.
- * @apiSuccess (200) {String} email  E-mail of the User.
- * @apiSuccess (200) {String} role  Role of the User.
+ * @apiSuccess  (200)   {MongoID}   _id         Id of the User.
+ * @apiSuccess  (200)   {String}    email       E-mail of the User.
+ * @apiSuccess  (200)   {String}    role        Role of the User.
+ * @apiSuccess  (200)   {Date}      createdAt   Date of the creation
+ * @apiSuccess  (200)   {Date}      updateAt    Date of last update
+ *
  * @apiSuccessExample {json} Success-Response:
  * HTTP/1.1 200 OK
  * {
@@ -26,9 +29,8 @@ var Account = require('./../models/account'),
  *  "updatedAt": "2016-03-04 20:09:24.000Z"
  * }
  *
- * @apiError (401) Unauthorized Unauthorized.
- * @apiError (404) NotFound Not Found.
- * @apiError (500) InternalServerError Internal Server Error.
+ * @apiUse 401
+ * @apiUse 500
  */
 exports.getAccount = (req, res)=> {
     Account.findOne()
@@ -42,23 +44,26 @@ exports.getAccount = (req, res)=> {
         });
 };
 /**
- * @api {get} /my/things Returns all things related to authenticated account
- * @apiDescription Returns all things related to authenticated account
+ * @api {get}   /my/things  Returns all things related to authenticated account
+ * @apiDescription  Returns all things related to authenticated account
  * @apiName GetThings
- * @apiGroup My
+ * @apiGroup    My
  *
- * @apiPermission user
- * @apiHeader {String} Authorization bearer User unique access-key.
+ * @apiPermission   user
+ * @apiUse  AuthenticationToken
  *
- * @apiParam {String} limit.
- * @apiParam {String} skip.
+ * @apiParam    {Number{0..}}   limit=20    How many results should be returned
+ * @apiParam    {Number{0..}}   skip=0      How many elements should be skipped
  *
- * @apiSuccess (200) {String} things[]._id Id of the Thing.
- * @apiSuccess (200) {String} things[].name Name of the Thing.
- * @apiSuccess (200) {String} things[].owner  Id of the User.
- * @apiSuccess (200) {String} things[].type  Type of the Thing.
- * @apiSuccess (200) {Number} skip. How many elements was skiped.
- * @apiSuccess (200) {Number} limit Results limited to.
+ * @apiSuccess  (200)   {Object[]{0..}} things              Collection of Thing objects
+ * @apiSuccess  (200)   {MongoID}       things._id          Id of the Thing
+ * @apiSuccess  (200)   {String}        things.name         Name of the Thing
+ * @apiSuccess  (200)   {String}        things.owner        Id of the Account
+ * @apiSuccess  (200)   {String}        things.type         Type of the Thing
+ * @apiSuccess  (200)   {Date}          things.createdAt    Date of the creation
+ * @apiSuccess  (200)   {Date}          things.updateAt     Date of last update
+ * @apiSuccess  (200)   {Number{0..}}   skip                How many elements was skipped
+ * @apiSuccess  (200)   {Number{0..}}   limit               To how many elements collection was limited
  *
  * @apiSuccessExample {json} Success-Response:
  * HTTP/1.1 200 OK
@@ -71,15 +76,15 @@ exports.getAccount = (req, res)=> {
  *    "type": "RECEPTOR",
  *    "createdAt": "2016-03-04 20:09:24.000Z",
  *    "updatedAt": "2016-03-04 20:09:24.000Z"
- *   }
+ *   },
+ *   ...
  *  ],
  *  "skip": 0,
  *  "limit": 20
  * }
  *
- * @apiError (401) Unauthorized Unauthorized.
- * @apiError (404) NotFound Not Found.
- * @apiError (500) InternalServerError Internal Server Error.
+ * @apiUse 401
+ * @apiUse 500
  */
 exports.getThings = (req, res)=> {
 
@@ -101,25 +106,24 @@ exports.getThings = (req, res)=> {
 };
 
 /**
- * @api {get} /my/things/count Returns number of things related to authenticated account
- * @apiDescription Returns number of things related to authenticated account
+ * @api {get}   /my/things/count    Returns number of things related to authenticated account
+ * @apiDescription  Returns number of things related to authenticated account
  * @apiName countMyThings
- * @apiGroup My
+ * @apiGroup    My
  *
- * @apiPermission user
- * @apiHeader {String} Authorization bearer User unique access-key.
+ * @apiPermission   user
+ * @apiUse  AuthenticationToken
  *
+ * @apiSuccess  (200)   {Number{0..}}   things  Number of things related to authenticated account
  *
- * @apiSuccess (200) {Number} things  Number of things related to authenticated account.
  * @apiSuccessExample {json} Success-Response:
  * HTTP/1.1 200 OK
  * {
  *  "things": 8
  * }
  *
- * @apiError (401) Unauthorized Unauthorized.
- * @apiError (404) NotFound Not Found.
- * @apiError (500) InternalServerError Internal Server Error.
+ * @apiUse 401
+ * @apiUse 500
  */
 exports.countMyThings = (req, res)=> {
     Thing.count()
@@ -135,31 +139,34 @@ exports.countMyThings = (req, res)=> {
 ;
 
 /**
- * @api {get} /my/tokens Returns all tokens related to authenticated account
- * @apiDescription Returns all tokens related to authenticated account
+ * @api {get}   /my/tokens  Returns all tokens related to authenticated account
+ * @apiDescription  Returns all tokens related to authenticated account
  * @apiName GetTokens
- * @apiGroup My
+ * @apiGroup    My
  *
- * @apiParam {String} limit.
- * @apiParam {String} skip.
+ * @apiParam    {Number{0..}}   limit=20    How many results should be returned
+ * @apiParam    {Number{0..}}   skip=0      How many elements should be skipped
  *
- * @apiPermission user
- * @apiHeader {String} Authorization bearer User unique access-key.
+ * @apiPermission   user
+ * @apiUse  AuthenticationToken
  *
- * @apiSuccess (200) {String} tokens[]._id   Id of the Token.
- * @apiSuccess (200) {String} tokens[].content  Content of the Token.
- * @apiSuccess (200) {String} tokens[].owner  Owner id of the Token.
- * @apiSuccess (200) {Boolean} tokens[].valid  Validation of the Token.
- * @apiSuccess (200) {Number} skip How many elements was skiped.
- * @apiSuccess (200) {Number} limit Results limited to.
+ * @apiSuccess  (200)   {Object[]{0..}} tokens              Collection of Token collection
+ * @apiSuccess  (200)   {MongoId}       tokens._id          Id of the Token
+ * @apiSuccess  (200)   {String}        tokens.token        Content of the Token
+ * @apiSuccess  (200)   {String}        tokens.owner        Owner id of the Token
+ * @apiSuccess  (200)   {Boolean}       tokens.valid        Validation of the Token
+ * @apiSuccess  (200)   {Date}          tokens.createdAt    Date of the creation
+ * @apiSuccess  (200)   {Date}          tokens.updateAt     Date of last update
+ * @apiSuccess  (200)   {Number{0..}}   skip                How many elements was skipped
+ * @apiSuccess  (200)   {Number{0..}}   limit               To how many elements collection was limited
  *
- * @apiSuccessExample {json} Success-Response:
+ * @apiSuccessExample   {json} Success-Response:
  * HTTP/1.1 200 OK
  * {
  *  "tokens": [
  *   {
  *    "_id": "5682773c21ba9d9736e8237b",
- *    "content": "c21ba9d9736e8237b.c21ba9d9736e8237b.c21ba9d9736e8237b",
+ *    "token": "c21ba9d9736e8237b.c21ba9d9736e8237b.c21ba9d9736e8237b",
  *    "owner": "5682773c21ba9d9736e8237b"
  *    "valid": true,
  *    "createdAt": "2016-03-04 20:09:24.000Z",
@@ -171,9 +178,8 @@ exports.countMyThings = (req, res)=> {
  *  "limit": 20
  * }
  *
- * @apiError (401) Unauthorized Unauthorized.
- * @apiError (404) NotFound Not Found.
- * @apiError (500) InternalServerError Internal Server Error.
+ * @apiUse 401
+ * @apiUse 500
  */
 exports.getTokens = (req, res)=> {
     Token.find()
@@ -194,25 +200,24 @@ exports.getTokens = (req, res)=> {
 };
 
 /**
- * @api {get} /my/tokens/count Returns number of tokens related to authenticated account
- * @apiDescription Returns  number of tokens related to authenticated account
- * @apiName countMYTokens
- * @apiGroup My
+ * @api {get}   /my/tokens/count    Returns number of tokens related to authenticated account
+ * @apiDescription  Returns number of tokens related to authenticated account
+ * @apiName countMyTokens
+ * @apiGroup    My
  *
- * @apiPermission user
- * @apiHeader {String} Authorization bearer User unique access-key.
+ * @apiPermission   user
+ * @apiUse  AuthenticationToken
  *
- * @apiSuccess (200) {Number} tokens  Number of tokens related to authenticated account.
- * @apiSuccessExample {json} Success-Response:
+ * @apiSuccess  (200)   {Number{0..}}    tokens  Number of tokens related to authenticated account
+ *
+ * @apiSuccessExample   {json} Success-Response:
  * HTTP/1.1 200 OK
  * {
- *  "tokens": 8,
-
+ *  "tokens": 8
  * }
  *
- * @apiError (401) Unauthorized Unauthorized.
- * @apiError (404) NotFound Not Found.
- * @apiError (500) InternalServerError Internal Server Error.
+ * @apiUse 401
+ * @apiUse 500
  */
 exports.countMyTokens = (req, res)=> {
     Token.count()
