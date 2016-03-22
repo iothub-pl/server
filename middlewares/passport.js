5969649fd6711de86cf9e21844f5a862ec307292
Przemysław Płyś <przemyslaw.plys@gmail.com>
2016-03-22 14:27:12 +0100

    Refactored passport middleware

--- middlewares/passport.js
+++ middlewares/passport.js
@@ -4,39 +4,37 @@ const Account = require('./../models/account');
 const Authentication = require('./../models/authentication');
 const authenticationHelper = require('./../helpers/authentication');
 
-var jwt = require('jsonwebtoken');
-var config = require('./../configs/app');
+const jwt = require('jsonwebtoken');
+const config = require('./../configs/app');
 
-exports.setup = (app)=> {
+exports.setup = (app) => {
     app.use(passport.initialize());
     passport.use(new BearerStrategy((token, done) => {
         Authentication
-            .findOne()
-            .select('+token')
+            .findOne().select('+token')
             .where('token').equals(token)
             .where('valid').equals(true)
             .then((authentication)=> {
-                authenticationHelper
+                return authenticationHelper
                     .verifyToken(authentication.getToken())
                     .then((decoded)=> {
-                        authentication
-                            .findOwner()
-                            .select('+salt')
-                            .select('+password')
+                        return authentication.findOwner()
+                            .select('+salt').select('+password')
                             .then((account) => {
-                                if (!account)
-                                    return done(null, false, {message: 'Unknown account.'});
-                                return done(null, account);
-                            })
-                            .catch((err)=> {
-                                if (err) return done(err);
+                                if (!account) {
+                                    return done(null, false, {
+                                        message: 'Unknown account.'
+                                    });
+                                } else {
+                                    return done(null, account);
+                                }
                             });
-                    }).catch((err)=> {
-                    if (err) return done(err);
-                });
+                    });
             })
             .catch((err)=> {
-                if (err) return done(err);
+                if (err) {
+                    return done(err);
+                }
             });
     }));
 };
